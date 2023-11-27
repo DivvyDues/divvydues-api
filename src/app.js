be2cb0c3a97703e6f294495156528db22146d8d4
Anacron-sec <michelebiondi01@gmail.com>
2023-11-27 11:35:19 +0100

    Refactored authentication route

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const fastify = require("fastify")({ logger: true });
-const { PrismaClient } = require("@prisma/client");
+
 const fastifySession = require("@fastify/session");
 const fastifyCookie = require("@fastify/cookie");
 const fastifyAuth = require("@fastify/auth");
@@ -8,8 +8,8 @@ const argon2Plugin = require("./plugins/fastify-argon2");
 
 const expenseSheetRoutes = require("./routes/expenseSheets");
 const healthCheckRoutes = require("./routes/healthcheck");
+const authenticationRoutes = require("./routes/authentication");
 
-const prisma = new PrismaClient();
 fastify.register(fastifyCookie);
 fastify.register(fastifySession, {
   cookie: { secure: false }, //TODO mechanism to set to true in prod
@@ -17,6 +17,8 @@ fastify.register(fastifySession, {
 });
 
 fastify.register(fastifyAuth);
+
+// Auth decorators
 
 fastify.decorate("verifyUserSession", function (request, reply, done) {
   //TODO Check if there are possible security risk (like a user session reuse after destroy)
@@ -26,59 +28,12 @@ fastify.decorate("verifyUserSession", function (request, reply, done) {
   done();
 });
 
+// Register Custom Plugins
 fastify.register(argon2Plugin);
 
+// Register Routes
 fastify.register(healthCheckRoutes);
-
-// User Registration Endpoint
-fastify.post("/register", async (request, reply) => {
-  const { username, password } = request.body;
-  //TODO Add rules for secure passwords
-
-  try {
-    const hashedPassword = await fastify.argon2.hash(password);
-    const user = await prisma.user.create({
-      data: {
-        username,
-        password: hashedPassword,
-      },
-    });
-
-    return { id: user.id, username: user.username };
-  } catch (error) {
-    reply.status(500).send({ error: error.message }); //TODO remove backend error messages
-  }
-});
-
-// User Login Endpoint
-fastify.post("/login", async (request, reply) => {
-  const { username, password } = request.body;
-
-  try {
-    const user = await prisma.user.findUnique({ where: { username } });
-    if (!user || !(await fastify.argon2.verify(user.password, password))) {
-      return reply.status(401).send({ error: "Invalid username or password" });
-    }
-
-    // Set up user session
-    request.session.user = { id: user.id, username: user.username };
-    return { message: "Logged in successfully" };
-  } catch (error) {
-    reply.status(500).send({ error: error.message });
-  }
-});
-
-// User Logout Endpoint
-fastify.post("/logout", async (request, reply) => {
-  request.session.destroy((err) => {
-    if (err) {
-      return reply.status(500).send({ error: "Failed to log out" });
-    }
-    reply.send({ message: "Logged out successfully" });
-  });
-});
-
-// Register routes
+fastify.register(authenticationRoutes);
 fastify.register(expenseSheetRoutes);
 
 const start = async () => {
---
+++ src/routes/authentication.js
@@ -0,0 +1,55 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+async function authenticationRoutes(fastify, options) {
+  fastify.post("/register", async (request, reply) => {
+    const { username, password } = request.body;
+    //TODO Add rules for secure passwords
+
+    try {
+      const hashedPassword = await fastify.argon2.hash(password);
+      const user = await prisma.user.create({
+        data: {
+          username,
+          password: hashedPassword,
+        },
+      });
+
+      return { id: user.id, username: user.username };
+    } catch (error) {
+      reply.status(500).send({ error: error.message }); //TODO remove backend error messages
+    }
+  });
+
+  // User Login Endpoint
+  fastify.post("/login", async (request, reply) => {
+    const { username, password } = request.body;
+
+    try {
+      const user = await prisma.user.findUnique({ where: { username } });
+      if (!user || !(await fastify.argon2.verify(user.password, password))) {
+        return reply
+          .status(401)
+          .send({ error: "Invalid username or password" });
+      }
+
+      // Set up user session
+      request.session.user = { id: user.id, username: user.username };
+      return { message: "Logged in successfully" };
+    } catch (error) {
+      reply.status(500).send({ error: error.message });
+    }
+  });
+
+  // User Logout Endpoint
+  fastify.post("/logout", async (request, reply) => {
+    request.session.destroy((err) => {
+      if (err) {
+        return reply.status(500).send({ error: "Failed to log out" });
+      }
+      reply.send({ message: "Logged out successfully" });
+    });
+  });
+}
+
+module.exports = authenticationRoutes;
