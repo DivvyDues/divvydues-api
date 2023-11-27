e716eef5c2b2f12fc953b9a7a37b21c22640ea01
Anacron-sec <michelebiondi01@gmail.com>
2023-11-27 17:46:48 +0100

    Refactored code to check if user is member of the sheet

--- src/plugins/decorators/auth-decorators.js
+++ src/plugins/decorators/auth-decorators.js
@@ -2,12 +2,43 @@
 const fp = require("fastify-plugin");
 
 async function authDecorators(fastify, options) {
-  fastify.decorate("verifyUserSession", function (request, reply, done) {
-    if (!request.session.user) {
-      return done(new Error("Not authenticated"));
-    }
-    done();
-  });
+  fastify
+    .decorate("verifyUserSession", function (request, reply, done) {
+      if (!request.session.user) {
+        return done(new Error("Not authenticated"));
+      }
+      done();
+    })
+    .decorate(
+      "verifyUserExpenseSheetMembership",
+      async function (request, reply, done) {
+        // Use same message to prevent leaking information about database structure
+        const errorMessage = "User is not a member of the expense sheet";
+
+        const userId = request.session.user.id;
+        const { expenseSheetId } = request.params;
+
+        const expenseSheet = await fastify.prisma.expenseSheet.findUnique({
+          where: { id: parseInt(expenseSheetId) },
+          include: { members: true },
+        });
+
+        if (!expenseSheet) {
+          reply.code(403);
+          done(new Error(errorMessage));
+        }
+
+        const existingMemberIds = expenseSheet.members.map(
+          (member) => member.id
+        );
+        const userIsMember = existingMemberIds.includes(userId);
+        if (!userIsMember) {
+          reply.code(403);
+          done(new Error(errorMessage));
+        }
+        done();
+      }
+    );
 }
 
 module.exports = fp(authDecorators);
