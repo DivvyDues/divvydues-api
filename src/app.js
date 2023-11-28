b418bae63a1b692fb3f2ea883f954727f4d30f8d
Anacron-sec <michelebiondi01@gmail.com>
2023-11-28 16:31:39 +0100

    Add note for future of dotenv in project

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config(); //TODO Research safer alternatives for production code
 const fastify = require("fastify")({ logger: true });
 
 // Register Core plugins
