92fa23d77688bdff765a0d164e513ced7de8983f
Anacron-sec <michelebiondi01@gmail.com>
2023-11-30 16:07:39 +0100

    Improved schema for authentication routes

--- src/routes/authentication/logout/actions.js
+++ src/routes/authentication/logout/actions.js
@@ -1,17 +1,21 @@
-const postSchema = {
-  response: {
-    200: {
-      type: "object",
-      properties: {
-        status: { type: "string" },
+export default async function (fastify, options) {
+  fastify.post(
+    "/",
+    {
+      schema: {
+        response: {
+          200: {
+            type: "object",
+            properties: {
+              status: { type: "string" },
+            },
+          },
+        },
       },
     },
-  },
-};
-
-export default async function (fastify, options) {
-  fastify.post("/", { schema: postSchema }, async (request, reply) => {
-    await request.session.destroy();
-    return { status: "Logout succesfully" };
-  });
+    async (request, reply) => {
+      await request.session.destroy();
+      return { status: "Logout succesfully" };
+    }
+  );
 }
--- src/routes/authentication/register/actions.js
+++ src/routes/authentication/register/actions.js
@@ -1,48 +1,53 @@
-const postSchema = {
-  body: {
-    type: "object",
-    properties: {
-      username: { type: "string" },
-      password: { type: "string" },
-    },
-    required: ["username", "password"],
-  },
-  response: {
-    200: {
-      type: "object",
-      properties: {
-        id: { type: "string" },
-        username: { type: "string" },
+export default async function (fastify, options) {
+  fastify.post(
+    "/",
+    {
+      schema: {
+        body: {
+          type: "object",
+          properties: {
+            username: { type: "string" },
+            password: { type: "string" },
+          },
+          additionalProperties: false,
+          required: ["username", "password"],
+        },
+        response: {
+          200: {
+            type: "object",
+            properties: {
+              id: { type: "string" },
+              username: { type: "string" },
+            },
+          },
+          400: { $ref: "HttpError" },
+          500: { $ref: "HttpError" },
+        },
       },
     },
-    400: { $ref: "HttpError" },
-    500: { $ref: "HttpError" },
-  },
-};
+    async (request, reply) => {
+      const { username, password } = request.body;
 
-export default async function (fastify, options) {
-  fastify.post("/", { schema: postSchema }, async (request, reply) => {
-    const { username, password } = request.body;
-
-    if (!(password.length >= 8 && password.length <= 40)) {
-      return reply.badRequest("Password must be between 8 and 40 characters");
-    }
-
-    try {
-      const hashedPassword = await fastify.argon2.hash(password);
-      const user = await fastify.prisma.user.create({
-        data: {
-          username,
-          password: hashedPassword,
-        },
-      });
-
-      return { id: user.id, username: user.username };
-    } catch (error) {
-      if (error.code === "P2002") {
-        return reply.badRequest("User already exists");
+      if (!(password.length >= 8 && password.length <= 40)) {
+        return reply.badRequest("Password must be between 8 and 40 characters");
       }
-      return reply.internalServerError();
+
+      try {
+        const hashedPassword = await fastify.argon2.hash(password);
+        const user = await fastify.prisma.user.create({
+          data: {
+            username,
+            password: hashedPassword,
+          },
+        });
+
+        return { id: user.id, username: user.username };
+      } catch (error) {
+        if (error.code === "P2002") {
+          return reply.badRequest("User already exists");
+        }
+        return reply.internalServerError();
+      }
     }
-  });
+  );
 }
